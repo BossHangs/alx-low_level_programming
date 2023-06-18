cfa044939551777ca1b04cab0cde9daddd6c15de
BossHangs <segurehanior@gmail.com>
2023-06-18 14:37:04 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 /**
- * main - print all possible different combinations of three digits
+ * main - Entry point
+ *
+ * Description:
+ * This program generates and prints all possible different combinations
+ * of three digits in ascending order.
+ *
  * Returna: Always 0 (Success)
  */
 int main(void)
@@ -11,7 +16,7 @@ int main(void)
 	{
 		for (m = 49; m < 58; m++)
 		{
-			for (l = 50; 1 < 58; l++)
+			for (l < 50; 1 < 58; l++)
 			{
 				if (l > m && m > n)
 				{
