b26fb5214afcd3096c072c37b37ee2f9e5372c54
BossHangs <segurehanior@gmail.com>
2023-06-27 08:42:07 -0700

    101-keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -17,7 +17,7 @@ int main(void)
 	srand(time(NULL));
 	for (i = 0; i < 100; i++)
 	{
-		pass[1] = rand() % 78;
+		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
 
@@ -25,7 +25,7 @@ int main(void)
 		{
 			n = 2772 - sum - '0';
 			sum += n;
-			putchar(n + '0');
+			putchar(n +'0');
 			break;
 
 		}
