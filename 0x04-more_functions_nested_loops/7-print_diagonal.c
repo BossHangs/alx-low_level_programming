10a370f743a272ab80c9e033ca6bc04f83bf577a
BossHangs <segurehanior@gmail.com>
2023-06-28 15:34:55 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *grint_diagonal - Printing lines diagonal
+ * @n: input variable
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+
+		putchar('\n');
+
+	for (i = 0; i < n; i++)
+
+	{
+		for (j = 0; j < i; j++)
+
+
+		{
+
+			putchar(' ');
+
+		}
+
+		putchar('\\');
+		putchar('\n');
+	}
+}
