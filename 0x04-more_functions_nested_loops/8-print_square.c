5533dc587e5be845b7b7a50f7c61f2d81992623e
BossHangs <segurehanior@gmail.com>
2023-06-28 16:46:39 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,27 +13,23 @@ void print_square(int size)
 	int i, j;
 
 	if (size <= 0)
-	{
 
+	{
 		putchar('\n');
-	return;
+		return;
 
 	}
 
 	for (i = 0; i < size; i++)
 
 	{
+		for (j = 0; j < size; j++)
 
-			for (j = 0; j < (size); j++)
+		{
+			putchar('0');
 
-			{
-				putchar('0');
+		}
 
-			}
-
-			putchar('\n');
-
+		putchar('\n');
 	}
 }
-
-
