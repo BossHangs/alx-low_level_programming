78a4f2b5f7e0fc95d9445cd1b5233344277a8f7d
BossHangs <segurehanior@gmail.com>
2023-06-28 04:38:25 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,18 +2,22 @@
 #include <stdio.h>
 
 /**
- * print_most_numberss -print 01356789
+ *print_most_numbers - Prints numbers from '0' to '9', excluding '2' and '4'
  *
- * Return:void
+ *This function prints the numbers from '0' to '9', excluding '2' and '4',
+ *using the putchar function. Each character is printed individually, and
+ *a newline character is printed at the end.
+ *Return:void
  */
 
 void print_most_numbers(void)
+
 {
 	char c;
 
 	for (c = '0' ; c <= '9'; c++)
 	{
-		if (!(c == '2' || c == '4'))
+		if (c != '2' &&  c != '4')
 			putchar(c);
 	}
 	putchar('\n');
