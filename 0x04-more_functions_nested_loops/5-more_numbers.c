a17b2ff2f47942ec1a86a75a1c4544117a771c1d
BossHangs <segurehanior@gmail.com>
2023-06-30 14:37:23 -0700

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,33 +2,24 @@
 #include <stdio.h>
 
 /**
- * more_numbers -print numbers from 0 to 14, ten times
- *
- * This function prints the numbers from 0 to 14 in a sequence, repeating it
- * ten times. Each number is printed individually, and a newline character is
- * printed after each sequence of numbers. The function uses nested loops to
- * control the repetition and ranges of numbers.
- */
+* more_numbers - print more numbers
+*/
 
-void more_number(void)
+
+void more_numbers(void)
+
 {
+
 	int i, j;
 
-
-	for (i = 0; i < 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
-
 		for (j = 0; j <= 14; j++)
 		{
-
-			if (j > 9)
-			{
-
-				putchar((j / 10) + '0');
-			}
-
-			putchar((j % 10) + '0');
+			if (j >= 10)
+			putchar('1');
+			_putchar (j % 10 + '0');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
